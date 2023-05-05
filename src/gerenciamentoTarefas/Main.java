0485a4a0ec490e4687b218c27ba79cf6690c484d
Abdiel <abdieldeathayde67@gmail.com>
2023-05-04 21:43:10 -0300

    atualizando sistema de gerenciamento de Tarefas em Java

--- src/gerenciamentoTarefas/Main.java
+++ src/gerenciamentoTarefas/Main.java
@@ -1,63 +1,36 @@
 package gerenciamentoTarefas;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.TimeZone;
-import java.util.List;
-import gerenciamentoTarefas.Tarefa;
+import java.lang.ProcessBuilder.Redirect.Type;
+import java.util.Scanner;
 
-@SuppressWarnings("unused")
 public class Main {
 
 	// criar métodos para criar remover e buscar tarefas
 
 	public static void main(String[] args) {
 		
-		Calendar data_inicio = Calendar.getInstance(TimeZone.getTimeZone("Brazil/East"));
-		Calendar data_conclusao = Calendar.getInstance();
-		
-//		data_inicio.getTime();
-		
-		int hora_inicio = data_inicio.get(Calendar.HOUR_OF_DAY);
-		int minuto_inicio = data_inicio.get(Calendar.MINUTE);
-		int segundo_inicio = data_inicio.get(Calendar.SECOND);
-		int hora_conclusao = data_conclusao.get(Calendar.HOUR_OF_DAY);
-		int minuto_conclusao = data_conclusao.get(Calendar.MINUTE);
-		int segundo_conclusao = data_conclusao.get(Calendar.SECOND);
-		
-		Tarefa tarefa1 = new Tarefa("Estudar Java", "Estudar POO em Java \n", hora_inicio , minuto_inicio , segundo_inicio , hora_conclusao , minuto_conclusao, segundo_conclusao );
-		Tarefa tarefa2 = new Tarefa("Estudar Java", "Estudar Estrutura de dados em Java \n", hora_inicio, minuto_inicio, segundo_inicio , hora_conclusao, minuto_conclusao, segundo_conclusao);
-		
-		List<Tarefa> tarefas = new ArrayList<>();
-		tarefas.add(tarefa1);
-		tarefas.add(tarefa2);
+		Scanner sc = new Scanner(System.in);
+		int opcao = 1;
+		opcao = sc.nextInt();
 		
 		
-		int hora_inicio2 = data_inicio.get(Calendar.HOUR_OF_DAY);
-		int minuto_inicio2 = data_inicio.get(Calendar.MINUTE);
-		int segundo_inicio2 = data_inicio.get(Calendar.SECOND);
-		int hora_conclusao2 = data_conclusao.get(Calendar.HOUR_OF_DAY);
-		int minuto_conclusao2 = data_conclusao.get(Calendar.MINUTE);
-		int segundo_conclusao2 = data_conclusao.get(Calendar.SECOND);
+		switch(opcao){
+			case 0:
+				break;
+			case 1:
+				Tarefa tarefa1 = new Tarefa("Estudar Java", "Estudar POO em Java");
+				Tarefa tarefa2 = new Tarefa("Estudar Java", "Estudar Estrutura de dados em Java");
+				Tarefa tarefa3 = new Tarefa("Estudar Java", "Estudar Padrões de Projetos em Java");
+				Tarefa tarefa4 = new Tarefa("Estudar Banco de dados", "Estudar banco de dados SQL Server");
+				Tarefa tarefa5 = new Tarefa("Estudar Spring", "Estudar Spring JPA Gradle");
+				
+				System.out.println("Tarefa1: " + tarefa1.nome + " Descrição: " + tarefa1.descricao + " \nTarefa2: " + tarefa2.nome + " Descrição: " + tarefa2.descricao + "\nTarefa3: " + tarefa3.nome + " Descrição: " + tarefa3.descricao + "\nTarefa4: " + tarefa4.nome + " Descrição: " + tarefa4.descricao + "\nTarefa5: " + tarefa5.nome + " Descrição: " + tarefa5.descricao);
+			case 2:
+				
 		
-//		data_conclusao.getTime();
-		
-		
-		System.out.println(tarefa1.toString());
-		System.out.println(tarefa2.toString());
-		
-		System.out.println("Tarefa1 \n" + hora_inicio + ":" + minuto_inicio + ":" + segundo_inicio );
-		System.out.println("Tarefa2 \n" + hora_inicio2 + ":" + minuto_inicio2 + ":" + segundo_inicio2);
-		
-		System.out.println("Tarefa1: " + tarefa1.nome + " Descrição: " + tarefa1.descricao + " \nTarefa2: " + tarefa2.nome + " Descrição: " + tarefa2.descricao);
-//		tarefa1.adicionarTarefa(null, null, data_inicio, data_conclusao)
-		
-//		Tarefa trf = new Tarefa();
-		tarefa1.exibirTarefa();
-//		System.out.print(Tarefa.exibirTarefa(tarefas));
-		
-		
+		}
 		
 	}
-	
-} 
+
+
+}
--- src/gerenciamentoTarefas/Tarefa.java
+++ src/gerenciamentoTarefas/Tarefa.java
@@ -1,42 +1,18 @@
 package gerenciamentoTarefas;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
 public class Tarefa {
 	
 	List<Tarefa> tarefa = new ArrayList<Tarefa>();
 	String nome;
 	String descricao;
-	int i;
-	int j; 
-	int k;
-	int l;
-	int m;
-	int n;
-	Calendar data_inicio = Calendar.getInstance();
-	Calendar data_conclusao = Calendar.getInstance();
 	
-	public Tarefa(String nome, String descricao, int i, int j, int k, int l, int m, int n) {
+	public Tarefa(String nome, String descricao) {
 		this.nome = nome;
 		this.descricao = descricao;
-		this.data_inicio = data_inicio;
-		this.data_conclusao = data_conclusao;
-		this.i = i;
-		this.j = j;
-		this.k = k;
-		this.l = l;
-		this.m = m;
-		this.n = n;
-	}
-
-	public Tarefa(String string, String string2, Date time) {
-		// TODO Auto-generated constructor stub
 	}
 
 	public String getNome() {
@@ -52,117 +28,41 @@ public class Tarefa {
 		this.descricao = descricao;
 	}
 
-	public Calendar getData_inicio() {
-		return data_inicio;
+	@Override
+	public int hashCode() {
+		return Objects.hash(descricao, nome, tarefa);
 	}
-	public void setData_inicio(Calendar data_inicio) {
-		this.data_inicio = data_inicio;
-	}
-	public Calendar getData_conclusao() {
-		return data_conclusao;
-	}
-	public void setData_conclusao(Calendar data_conclusao) {
-		this.data_conclusao = data_conclusao;
-	}
-		
-	public int getI() {
-		return i;
-	}
-	
-	public void setI(int i) {
-		this.i = i;
-	}
-
-	public int getJ() {
-		return j;
-	}
-
-	public void setJ(int j) {
-		this.j = j;
-	}
-
-	public int getK() {
-		return k;
-	}
-
-	public void setK(int k) {
-		this.k = k;
-	}
-
-	public int getL() {
-		return l;
-	}
-
-	public void setL(int l) {
-		this.l = l;
-	}
-
-	public int getM() {
-		return m;
-	}
-
-	public void setM(int m) {
-		this.m = m;
-	}
-
-	public int getN() {
-		return n;
-	}
-
-	public void setN(int n) {
-		this.n = n;
-	}
-
-	public Tarefa adicionarTarefa(String nome, String descricao, Calendar data_inicio, Calendar data_conclusao) {
-		data_inicio = Calendar.getInstance();
-		Tarefa tarefa1 = new Tarefa("Estudar Java", "Estudar Padrões de Projetos em Java \n", data_inicio.getTime() );
-		Tarefa tarefa2 = new Tarefa("Estudar Banco de dados", "Estudar banco de dados SQL Server \n", data_inicio.getTime());
-		Tarefa tarefa3 = new Tarefa("Estudar Spring", "Estudar Spring JPA Gradle \n", data_inicio.getTime());
-		tarefa.add(tarefa1);
-		tarefa.add(tarefa2);
-		tarefa.add(tarefa3);
-		
-		System.out.println("Tarefa1: " + tarefa1.nome + "Descricao" + tarefa1.descricao + " \nTarefa2: " + tarefa2.nome + " Descricao: " + tarefa2.descricao + " \nTarefa3: " + tarefa3.nome + " Descricao" + tarefa3.descricao);
-		return tarefa3;		
-		
-	}
-	
-	public void exibirTarefa() {
-//		final DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-//		final Calendar cal = Calendar.getInstance();
-//		System.out.println(df.format(cal.getTime())); 
-		
-		System.out.printf("Tarefas: " + tarefa);
-		
-		
-//		System.out.println(adicionarTarefa("Estudar Matematica", "Estudar Juros", 2023/05/03, 2023/05/23));
-		
-		Iterator<Tarefa> it = tarefa.iterator();
-		
-		
-		while (it.hasNext()) {
-			System.out.println(it.next());
-		}
-//		for (int i = 0; i < tarefa.size(); i++) {
-////			String collection = tarefa.get(i).toString();
-//			System.out.println("Tarefas: " + tarefa.get(i));
-//		}
-//		return (Tarefa) tarefa;
-	}
-
 
 	@Override
-	public String toString() {
-		return "Tarefa [nome=" + nome + ", descricao=" + descricao + ", data_inicio=" + data_inicio
-				+ "\n"  + ", data_conclusao=" + data_conclusao + "]";
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tarefa other = (Tarefa) obj;
+		return Objects.equals(descricao, other.descricao) && Objects.equals(nome, other.nome)
+				&& Objects.equals(tarefa, other.tarefa);
 	}
 	
-	
-	
-	
-	
-	
-		
-	
 
+//	public Tarefa adicionarTarefa(String nome, String descricao, Calendar data_inicio, Calendar data_conclusao) {
+//		data_inicio = Calendar.getInstance();
+//		Tarefa tarefa1 = new Tarefa("Estudar Java", "Estudar Padrões de Projetos em Java \n" );
+//		Tarefa tarefa2 = new Tarefa("Estudar Banco de dados", "Estudar banco de dados SQL Server \n");
+//		Tarefa tarefa3 = new Tarefa("Estudar Spring", "Estudar Spring JPA Gradle \n");
+//		tarefa.add(tarefa1);
+//		tarefa.add(tarefa2);
+//		tarefa.add(tarefa3);
+//		
+//		System.out.println("Tarefa1: " + tarefa1.nome + "Descricao" + tarefa1.descricao + " \nTarefa2: " + tarefa2.nome + " Descricao: " + tarefa2.descricao + " \nTarefa3: " + tarefa3.nome + " Descricao" + tarefa3.descricao);
+//		return tarefa3;				
+//	}
+
+//	@Override
+//	public String toString() {
+//		return "Tarefa [nome=" + nome + ", descricao=" + descricao + ", data_inicio=" + data_inicio
+//				+ "\n"  + ", data_conclusao=" + data_conclusao + "]";
+//	}
 }
